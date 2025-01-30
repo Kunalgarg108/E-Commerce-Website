c320d304d6c4dd2e23b5bc6ef294ededbb274592
KUNAL KUMAR  GARG <123102108@nitkkr.ac.in>
2025-01-30 23:31:18 +0530

    Changes frontend

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from "../assets/assets";
+import NewsLetterBox from '../components/NewsLetterBox';
 function Contact() {
   return (
     <div>
@@ -12,12 +13,15 @@ function Contact() {
           <div className='flex flex-col justify-center items-start gap-6'>
             <p className='font-semibold text-xl text-gray-600'>Our Store</p>
             <p className='text-gray-500'>4355 Wills Station <br />Suits 350,Haryana,India</p>
-            <p></p>
-            <p></p>
-            <p></p>
-
+            <p className='text-gray-500'>Tel: (421) 551-213-4523 <br />
+            Email Id:rsdgtsgr@gmail.com
+            </p>
+            <p className='font-medium text-xl text-gray-500'> Careers at My Store</p>
+            <p className='text-gray-500 text-xl'>Learn more about our teams and job openings</p>
+            <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
           </div>
       </div>
+      <NewsLetterBox/>
     </div>
   )
 }
