cfb01d85996f72d4956a35ccd8aa0b58f819a07b
KUNAL KUMAR  GARG <123102108@nitkkr.ac.in>
2025-03-13 13:53:38 +0530

    added sidebar and navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
 import Navbar from './componenets/Navbar'
-
+import Sidebar from './componenets/Sidebar'
+import {Routes , Route} from 'react-router-dom'
 function App() {
 
   return (
-     <>
+     <div className='bg-gray-100 min-h-screen'>
+      <>
         <Navbar />
-     </>
+        <hr />
+        <div className='flex w-full'>
+             <Sidebar />
+             <div className='w-[82%] mx-auto ml-[max(5vw,25px)] my-6 text-gray-600 text-base'>
+                
+             </div>
+        </div>
+      </>
+     </div>
   )
 }
 
--- admin/src/componenets/Navbar.jsx
+++ admin/src/componenets/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import {assets} from '../assets/assets'
 function Navbar() {
   return (
-    <div>
-      <img src={assets.logo} alt="" />
-      <button>Logout</button>
+    <div className='flex item-center justify-between px-5 '>
+      <img src={assets.logo} alt="" className='w-[max(10%,80px)]'/>
+      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 rounded-full text-sm sm:text-sm'>Logout</button>
     </div>
   )
 }
